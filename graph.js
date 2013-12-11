bd9d8116a280f4c0b40fe2f4fb505dad1374efcb
Ilya Khokhryakov <ilyahohryakov@gmail.com>
2013-12-11 08:22:47 +0400

    Implemented cycles detection

--- graph.js
+++ graph.js
@@ -14,12 +14,53 @@
     function Graph(functions, dependencies) {
         this.functions = functions;
         this.dependencies = dependencies;
-        this.validate();
+
+        this.detectCycles();
         this.compile();
     }
 
-    Graph.prototype.validate = function () {
-        // TODO check for cycles
+    Graph.prototype.detectCycles = function () {
+        var key, i, edges, nodesWithNoEdges, node;
+
+        edges = {};
+        nodesWithNoEdges = [];
+
+        for (key in this.dependencies) {
+            if (this.dependencies.hasOwnProperty(key)) {
+                for (i = 0, edges[key] = []; i < this.dependencies[key].length; i += 1) {
+                    if (this.functions.hasOwnProperty(this.dependencies[key][i])) {
+                        edges[key].push(this.dependencies[key][i]);
+                    }
+                }
+
+                if (edges[key].length === 0) {
+                    nodesWithNoEdges.push(key);
+                }
+            }
+        }
+
+        while (nodesWithNoEdges.length > 0) {
+            node = nodesWithNoEdges[0];
+            nodesWithNoEdges.splice(0, 1);
+
+            for (key in edges) {
+                if (edges.hasOwnProperty(key)) {
+                    i = edges[key].indexOf(node);
+                    if (i !== -1) {
+                        edges[key].splice(i, 1);
+                        if (edges[key].length === 0) {
+                            nodesWithNoEdges.push(key);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (key in edges) {
+            if (edges.hasOwnProperty(key) && edges[key].length !== 0) {
+                throw new Error('Cannot compile cyclic graph');
+            }
+        }
     };
 
     Graph.prototype.compile = function () {
@@ -62,8 +103,7 @@
     };
 
     Graph.prototype.compute = function (key, data) {
-        // TODO check for key presence
-        // TODO check for missing dependencies
+        // TODO check for key presence and missing dependencies
     };
 
     function LazyGraph() {
